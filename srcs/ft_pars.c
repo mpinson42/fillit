eb1c42fb7ebd597f54d18b25a811263c6d00f20c
Mathieu PINSON <mpinson@e1r1p3.42.fr>
2017-03-09 20:44:05 +0100

    mis a jour non fini je vais miam

--- srcs/ft_pars.c
+++ srcs/ft_pars.c
@@ -1,88 +1,110 @@
 #include "fillit.h"
 
+void ft_htag(int bol2, int i, int j, t_env *e)
+{
+	int x;
+	int y;
+	if(bol2 == 0)
+	{
+		x = j;
+		y = i;
+	}
+	if(bol2 == 1)
+	{
+		
+		e->htag1[0] = j - x;
+		e->htag1[1] = i - y;
+	}
+	if(bol2 == 2)
+	{
+		e->htag2[0] = j - x;
+		e->htag2[1] = i - y;
+	}
+	if(bol2 == 3)
+	{
+		e->htag3[0] = j - x;
+		e->htag3[1] = i - y;
+	}	
+}
+
+void ft_adj(t_env *e, t_pars *p)
+{
+	while(p->str[p->i] != NULL)
+	{
+		p->j = 0;
+		while(p->str[p->i][p->j] != '\0')
+		{
+			if(p->str[p->i][p->j] == '#')
+			{
+				p->count_htag++;
+				ft_htag(p->bol2, p->i + 1, p->j + 1, e);
+				p->bol2++;
+			}
+			if(p->str[p->i][p->j] == '.')
+				p->count_dot++;
+			if(p->str[p->i][p->j] == '#' && p->i != 0 && p->str[p->i - 1][p->j] == '#')
+				p->count_adj++;
+			if(p->str[p->i][p->j] == '#' && p->j != 0 && p->str[p->i][p->j - 1] == '#')
+				p->count_adj++;
+			if(p->str[p->i][p->j] == '#' && p->i != 3 && p->str[p->i + 1][p->j] == '#')
+				p->count_adj++;
+			if(p->str[p->i][p->j] == '#' && p->j != 3 && p->str[p->i][p->j + 1] == '#')
+				p->count_adj++;
+			p->j++;
+		}
+		p->i++;
+	}
+}
+
+void ft_suit(t_pars *p, t_env *e, char *buff)
+{
+	p->bol2 = 0;
+	p->count_dot = 0;
+	p->count_adj = 0;
+	p->count_htag = 0;
+	if(p->count != 21 && p->count != 20)
+		p->bol = 2;
+	if(buff[4] != '\n' || buff[9] != '\n' || buff[14] != '\n' || buff[19] != '\n')
+		p->bol = 3;
+	p->str = ft_strsplit(buff, '\n');
+	p->i = 0;
+	if(p->bol3 == 1)
+	{
+		p->tmp2 = e;
+		if(!(e->next = (t_env *)malloc(sizeof(t_env))))
+			p->bol = 42;
+		e = e->next;
+		e->presed = p->tmp2;
+	}
+	p->bol3 = 1;
+	ft_adj(e, p);
+	if(p->count_adj != 8 && p->count_adj != 6)
+		p->bol = 5;
+	if(p->count_htag != 4)
+		p->bol = 6;
+	if(p->count_dot != 12)
+		p->bol = 7;
+}
+
 int ft_pars(t_env *e, char *file)
 {
-	int fd;
+	int fd; 
 	char *buff;
-	char **str;
-	int count;
-	int bol;
-	int i;
-	int j;
-	int count_adj;
-	int count_htag;
-	int count_dot;
-	int bol2;
+	t_pars p;
 
-	i = 0;
-	j = 0;
-	bol = 0;
+	p.bol3 = 0;
+	p.bol = 0;
+	p.tmp = e;
 	fd = open(file, O_RDONLY);
 	if(fd == -1)
 	{
 		ft_putstr("open failed");
-		bol = 1;
+		p.bol = 1;
 	}
-	while((count = read(fd, buff, 21)) > 0)
+	while((p.count = read(fd, buff, 21)) > 0)
 	{
-		bol2 = 0;
-		count_dot = 0;
-		count_adj = 0;
-		count_htag = 0;
-		if(count != 21 && count != 20)
-			bol = 2;
-		if(buff[4] != '\n' || buff[9] != '\n' || buff[14] != '\n' || buff[19] != '\n')
-			bol = 3;
-		str = ft_strsplit(buff, '\n');
-		i = 0;
-		while(str[i] != NULL)
-		{
-			j = 0;
-			while(str[i][j] != '\0')
-			{
-				if(str[i][j] == '.')
-					count_dot++;
-				if(str[i][j] == '#')
-				{
-					count_htag++;
-					if(bol2 == 1)
-					{
-						e->htag1[0] =
-						e->htag1[1] =
-					}
-					if(bol2 == 2)
-					{
-						e->htag2[0] =
-						e->htag2[1] =
-					}
-					if(bol3 == 3)
-					{
-						e->htag3[0] =
-						e->htag3[1] =
-					}
-					bol2++;
-				}
-				if(str[i][j] == '#' && i != 0 && str[i - 1][j] == '#')
-					count_adj++;
-				if(str[i][j] == '#' && j != 0 && str[i][j - 1] == '#')
-					count_adj++;
-				if(str[i][j] == '#' && i != 3 && str[i + 1][j] == '#')
-					count_adj++;
-				if(str[i][j] == '#' && j != 3 && str[i][j + 1] == '#')
-					count_adj++;
-				j++;
-			}
-			i++;
-		}
-		if(count_adj != 8 && count_adj != 6)
-			bol = 5;
-		if(count_htag != 4)
-			bol = 6;
-		if(count_dot != 12)
-			bol = 7;
-		//printf("%d\n", count_htag);
-		//printf("%s", buff);
+		ft_suit(&p, e , buff);
 	}
-	printf("\n\n\n%d", bol);
-	return(bol);
+	e = p.tmp;
+	return(p.bol);
 }
